810d9fe63fc0c88916de1e60cef499b71f188cd7
NamKiHyeok <NamKiHyeok@DESKTOP-V8FTS1P>
2021-11-20 10:25:10 +0900

    Array List

--- src/list/arraylist/implementation/ArrayList.java
+++ src/list/arraylist/implementation/ArrayList.java
@@ -100,5 +100,10 @@ public class ArrayList {
 		public void add(Object element) {
 			ArrayList.this.add(nextIndex++, element);
 		}
+		
+		public void remove() {
+			ArrayList.this.remove(nextIndex-1);
+			nextIndex--;
+		}
 	}
 }
