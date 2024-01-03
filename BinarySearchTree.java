58044f0627fa74370fc1b884914913786d69054a
KiHyeok Nam <75459974+carpediem3651@users.noreply.github.com>
2024-01-03 10:46:44 +0900

    삭제할 Node탐색

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,17 +1,17 @@
+public class Node {
+    Node left;
+    Node right;
+    int value;
+
+    public Node(int data) {
+        this.value = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
 public class BinarySearchTree {
     Node head = null;
-
-    public class Node {
-        Node left;
-        Node right;
-        int value;
-        
-        public Node (int data) {
-            this.value = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
 
     public boolean insertNode(int data) {
         // CASE1. Node가 하나도 없을 떄
@@ -20,18 +20,18 @@ public class BinarySearchTree {
         } else {
             // CASE2. Node가 하나 이상 들어가 있을 때
             Node findNode = this.head;
-            while(true) {
+            while (true) {
                 // CASE2-1. 현재 Node의 왼쪽에 Node가 들어가야할 때
                 if (data < findNode.value) {
-                    if(findNode.left != null) {
+                    if (findNode.left != null) {
                         findNode = findNode.left;
                     } else {
                         findNode.left = new Node(data);
                         break;
                     }
-                // CASE2-2. 현재 Node의 오른쪽에 Node가 들어가야할 때
+                    // CASE2-2. 현재 Node의 오른쪽에 Node가 들어가야할 때
                 } else {
-                    if(findNode.right != null) {
+                    if (findNode.right != null) {
                         findNode = findNode.right;
                     } else {
                         findNode.right = new Node(data);
@@ -46,13 +46,13 @@ public class BinarySearchTree {
     // 탐색
     public Node search(int data) {
         // CASE1: Node가 하나도 없을 때
-        if(this.head==null){
+        if (this.head == null) {
             return null;
-        // CASE2: Node가 하나 이상 있을 때
+            // CASE2: Node가 하나 이상 있을 때
         } else {
             Node findNode = this.head;
-            while(findNode != null) {
-                if(findNode.value == data) {
+            while (findNode != null) {
+                if (findNode.value == data) {
                     return findNode;
                 } else if (data < findNode.value) {
                     findNode = findNode.left;
@@ -62,5 +62,41 @@ public class BinarySearchTree {
             }
             return null;
         }
+    }
+
+    public boolean delete(int value) {
+        boolean searched = false;
+
+        Node currParentNode = this.head;
+        Node currNode = this.head;
+
+        // 코너 케이스1: Node가 하나도 없을 때
+        if(this.head == null) {
+            return false;
+        } else {
+            // 코너 케이스2: Node가 하나이고 해당 Node가 삭제할 Node일 때
+            if(this.head.value == value && this.head.left == null && this.head.right == null) {
+                this.head = null;
+                return true;
+            }
+
+            while (currNode != null) {
+                if(currNode.value == value) {
+                    searched = true;
+                    break;
+                } else if (value < currNode.value) {
+                    currParentNode = currNode;
+                    currNode = currNode.left;
+                } else {
+                    currParentNode = currNode;
+                    currNode = currNode.right;
+                }
+            }
+
+            if(searched == false) {
+                return false;
+            }
+        }
+        
     }
 }
