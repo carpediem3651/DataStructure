cbe29d50b16c0de0be9db9957683165c4021f8a6
KiHyeok Nam <75459974+carpediem3651@users.noreply.github.com>
2024-01-09 09:44:56 +0900

    HashFuction구현

--- MyHash.java
+++ MyHash.java
@@ -1,4 +1,18 @@
-public class HashTable {
+public class MyHash {
     public Slot[] hashTable;
-    
+
+    public MyHash(Integer size) {
+        this.hashTable = new Slot[size]
+    }
+
+    public class Slot {
+        String value;
+        Slot(String value) {
+            this.value = value;
+        }
+    }
+
+    public Integer hashFunc(String key) {
+        return (int)(key.charAt(0))%this.hashTable.length;
+    }
 }
