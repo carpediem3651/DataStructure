7aa32022f91da8eee785bd63094a262863460c78
KiHyeok Nam <75459974+carpediem3651@users.noreply.github.com>
2024-01-09 09:54:08 +0900

    saveData함수 구현

--- MyHash.java
+++ MyHash.java
@@ -12,7 +12,22 @@ public class MyHash {
         }
     }
 
+    // Division 기법을 사용하여 hashFunction을 만든다. 
+    // 많은 기법이 있겠지만, key값의 맨 처음 글자를 가져와 hashtable의 총 갯수로 나눈 나머지를
+    // 반환하게하여 index값을 만드는 hashFuction을 만든다.
     public Integer hashFunc(String key) {
         return (int)(key.charAt(0))%this.hashTable.length;
     }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            this.hashTable[address].value = value;
+        } else {
+            this.hashTable[address] = new Slot(value);
+        }
+        return true;
+    }
+
+    
 }
