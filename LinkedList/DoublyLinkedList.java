6579cd27f8aaefa50cdc3bfea8c3e9b96191fc5f
KiHyeok Nam <75459974+carpediem3651@users.noreply.github.com>
2024-01-15 10:47:48 +0900

    feat: DoublyLinkedList 검색 구현

--- LinkedList/DoublyLinkedList.java
+++ LinkedList/DoublyLinkedList.java
@@ -40,15 +40,50 @@ public class DoublyLinkedList<T> {
         }
     }
 
+    public T searchFromHead(T isData) {
+        if(this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == isData) {
+                    return node.data;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public T searchFromTail(T isData) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.tail;
+            while (node != null) {
+                if (node.data == isData) {
+                    return node.data;
+                } else {
+                    node = node.prev;
+                }
+            }
+            return null;
+        }
+    }
+
     // 테스트
     public static void main(String[] args) {
         DoublyLinkedList<Integer> MyLinkedList = new DoublyLinkedList<Integer>();
-
+            // 테스트 1. 데이터 넣기
             MyLinkedList.addNode(2);
             MyLinkedList.addNode(4);
             MyLinkedList.addNode(5);
             MyLinkedList.addNode(8);
             MyLinkedList.addNode(3);
+
+            // 테스트 2. 데이터 찾기
+            MyLinkedList.searchFromHead(2);
 
             MyLinkedList.printAll();
     }
