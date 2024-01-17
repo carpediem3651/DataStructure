4ae63efbf303363b0d2af63d38c24fb24c6ecc40
KiHyeok Nam <75459974+carpediem3651@users.noreply.github.com>
2024-01-17 09:51:20 +0900

    feat: DoublyLinkedList 특정 노드에 노드 끼워넣기

--- LinkedList/DoublyLinkedList.java
+++ LinkedList/DoublyLinkedList.java
@@ -41,7 +41,7 @@ public class DoublyLinkedList<T> {
     }
 
     public T searchFromHead(T isData) {
-        if(this.head == null) {
+        if (this.head == null) {
             return null;
         } else {
             Node<T> node = this.head;
@@ -72,19 +72,69 @@ public class DoublyLinkedList<T> {
         }
     }
 
+    public boolean insertToFront(T existedData, T addData) {
+        if (this.head == null) {
+            this.head = new Node<T>(addData);
+            this.tail = this.head;
+        } else if (this.head.data == existedData) {
+            Node<T> newHead = new Node<T>(addData);
+            newHead.next = this.head;
+            this.head = newHead;
+            return true;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == existedData) {
+                    Node<T> nodePrev = node.prev;
+
+                    nodePrev.next = new Node<T>(addData);
+                    nodePrev.next.next = node;
+
+                    nodePrev.next.prev = nodePrev;
+                    node.prev = nodePrev.next;
+                    return true;
+                } else {
+                    node = node.next;
+                }
+            }
+        }
+        return false;
+    }
+
     // 테스트
     public static void main(String[] args) {
         DoublyLinkedList<Integer> MyLinkedList = new DoublyLinkedList<Integer>();
-            // 테스트 1. 데이터 넣기
-            MyLinkedList.addNode(2);
-            MyLinkedList.addNode(4);
-            MyLinkedList.addNode(5);
-            MyLinkedList.addNode(8);
-            MyLinkedList.addNode(3);
+        // // 테스트 1. 데이터 넣기
+        // MyLinkedList.addNode(2);
+        // MyLinkedList.addNode(4);
+        // MyLinkedList.addNode(5);
+        // MyLinkedList.addNode(8);
+        // MyLinkedList.addNode(3);
 
-            // 테스트 2. 데이터 찾기
-            MyLinkedList.searchFromHead(2);
+        // // 테스트 2. 데이터 찾기
+        // MyLinkedList.searchFromHead(2);
 
-            MyLinkedList.printAll();
+        // 테스트 3.특정 노드 끼워넣기
+        MyLinkedList.addNode(1);
+        MyLinkedList.addNode(2);
+        MyLinkedList.addNode(3);
+        MyLinkedList.addNode(4);
+        MyLinkedList.addNode(5);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
+
+        MyLinkedList.insertToFront(3, 2);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
+
+        MyLinkedList.insertToFront(6, 2);
+        MyLinkedList.insertToFront(1, 0);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
+
+        MyLinkedList.addNode(6);
+        MyLinkedList.printAll();
+
+        MyLinkedList.printAll();
     }
 }
